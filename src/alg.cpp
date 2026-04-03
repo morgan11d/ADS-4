eb1154fb371721b09db47df500e360f764accbd4
morgan11d <morkicel@gmail.com>
2026-04-03 20:37:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,15 @@
 // alg.cpp
+// Copyright 2024 <Your Name>
+
 #include <algorithm>
 
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
     for (int j = i + 1; j < len; ++j) {
-      if (arr[i] + arr[j] == value)
+      if (arr[i] + arr[j] == value) {
         count++;
+      }
     }
   }
   return count;
@@ -25,10 +28,11 @@ int countPairs2(int *arr, int len, int value) {
       if (arr[mid] == target) {
         firstPos = mid;
         right = mid - 1;
-      } else if (arr[mid] < target)
+      } else if (arr[mid] < target) {
         left = mid + 1;
-      else
+      } else {
         right = mid - 1;
+      }
     }
 
     if (firstPos != -1) {
@@ -40,10 +44,11 @@ int countPairs2(int *arr, int len, int value) {
         if (arr[mid] == target) {
           lastPos = mid;
           left = mid + 1;
-        } else if (arr[mid] < target)
+        } else if (arr[mid] < target) {
           left = mid + 1;
-        else
+        } else {
           right = mid - 1;
+        }
       }
       count += (lastPos - firstPos + 1);
     }
@@ -78,10 +83,11 @@ int countPairs3(int *arr, int len, int value) {
         left++;
         right--;
       }
-    } else if (sum < value)
+    } else if (sum < value) {
       left++;
-    else
+    } else {
       right--;
+    }
   }
   return count;
 }
